559a161d758bb6f059c68e5dec62cdba65d93bb2
José Bestel <josegbestel@gmail.com>
2019-08-16 09:39:26 -0300

    classe teste

---
+++ PandoriaMiniChallenge1/Classe.swift
@@ -0,0 +1,9 @@
+//
+//  Classe.swift
+//  PandoriaMiniChallenge1
+//
+//  Created by José Guilherme Bestel on 16/08/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import Foundation
