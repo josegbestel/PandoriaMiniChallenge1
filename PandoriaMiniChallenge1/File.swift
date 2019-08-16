beb4f56b2c0639b989bc67f11b6eb2dfee2064ce
Lucas Ronnau <lucas.ronnau2@gmail.com>
2019-08-16 09:43:52 -0300

    commit inicial

---
+++ PandoriaMiniChallenge1/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  PandoriaMiniChallenge1
+//
+//  Created by Lucas Ronnau on 16/08/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import Foundation
